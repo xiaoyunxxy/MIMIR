c18ecd4e9274890d0cc4acb68ba80d2fa066252a
xxu <xiaoyun.xu@ru.nl>
2023-10-22 23:27:57 +0200

    x

--- advmae_jobscript_cifar10.sh
+++ advmae_jobscript_cifar10.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
-#SBATCH --job-name preft_c_edm
+#SBATCH --job-name ft_edmvit
 #SBATCH --partition=icis
 #SBATCH --account=icis
 #SBATCH --qos=icis-preempt
 #SBATCH --mem=150G
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=64
 #SBATCH --time=24:00:00
 #SBATCH --output=./slurm_log/my-experiment-%j.out
@@ -17,18 +17,18 @@ cd /home/xxu/adv_mae
 
 
 # Hyper parameters
-num_gpu=4
+num_gpu=2
 mae_model=mae_vit_small
 vit_model=vit_small
 
 dataset=cifar10
 nb_classes=10
 patch_size=2
-input_size=32
+input_size=64
 data_root=../data
 
 pre_batchsize=256
-ft_batchsize=32
+ft_batchsize=64
 
 pre_blr=1.5e-4
 ft_blr=0.05
@@ -37,25 +37,25 @@ pre_output_dir=./experiment/${mae_model}_${dataset}_adv_fast_hsicpretrain_edm
 finetune_checkpoint=$pre_output_dir/checkpoint-799.pth
 ft_output_dir=./experiment/${mae_model}_${dataset}_advedmfinetune_with_adv_fast_hsicpretrain_edm
 
-m_port=1239
+m_port=1233
 
 # Pretrain
 
-mkdir -p $pre_output_dir
+# mkdir -p $pre_output_dir
 
-OMP_NUM_THREADS=2 python -m torch.distributed.launch --master_port $m_port --nproc_per_node=$num_gpu pretrain.py \
---batch_size $pre_batchsize \
---model $mae_model \
---norm_pix_loss --mask_ratio 0.75 \
---epochs 800 --warmup_epochs 40 \
---blr $pre_blr --weight_decay 0.05 \
---dataset "$dataset" --patch_size $patch_size \
---data_root $data_root --input_size $input_size \
---output_dir "$pre_output_dir" --log_dir "$pre_output_dir" \
---attack pgd_mae --steps 1 --eps 10 --alpha 8 \
---num_workers 16 \
---use_edm --aux_data_filename "/ceph/dis-ceph/xxu/edm_data/cifar10/5m.npz" \
---mi_train hsic --mi_xpl 0.00001 > "${pre_output_dir}/printlog" 2>&1
+# OMP_NUM_THREADS=2 python -m torch.distributed.launch --master_port $m_port --nproc_per_node=$num_gpu pretrain.py \
+# --batch_size $pre_batchsize \
+# --model $mae_model \
+# --norm_pix_loss --mask_ratio 0.75 \
+# --epochs 800 --warmup_epochs 40 \
+# --blr $pre_blr --weight_decay 0.05 \
+# --dataset "$dataset" --patch_size $patch_size \
+# --data_root $data_root --input_size $input_size \
+# --output_dir "$pre_output_dir" --log_dir "$pre_output_dir" \
+# --attack pgd_mae --steps 1 --eps 10 --alpha 8 \
+# --num_workers 16 \
+# --aux_data_filename "/ceph/dis-ceph/xxu/edm_data/cifar10/5m.npz" \
+# --mi_train hsic --mi_xpl 0.00001 > "${pre_output_dir}/printlog" 2>&1
 
 
 # Finetune
@@ -72,10 +72,10 @@ OMP_NUM_THREADS=2 python -m torch.distributed.launch --master_port $m_port --npr
  --blr $ft_blr \
  --layer_decay 0.65 \
  --weight_decay 0.05 --drop_path 0.1 \
- --reprob 0.25 --use_edm \
- --aux_data_filename "/ceph/dis-ceph/xxu/edm_data/cifar10/5m.npz" \
+ --reprob 0.25 \
+ --use_edm --aux_data_filename "/ceph/dis-ceph/xxu/edm_data/cifar10/5m.npz" \
  --data_root $data_root \
  --dataset "$dataset" --nb_classes $nb_classes \
  --patch_size $patch_size --input_size $input_size \
  --attack_train pgd \
- --num_workers 16 > "${ft_output_dir}/printlog" 2>&1
+ --num_workers 0 > "${ft_output_dir}/printlog" 2>&1
