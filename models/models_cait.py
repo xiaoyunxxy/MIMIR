16ce3b5f682036f90d72b0f3de4cfbdb18f0d071
xxu <xiaoyun.xu@ru.nl>
2023-11-11 10:52:32 +0100

    more heads

--- models/models_cait.py
+++ models/models_cait.py
@@ -1,8 +1,20 @@
 from timm.models.cait import Cait
 import timm.models.cait
 
+model_args_xxs24_mh = dict(patch_size=16, embed_dim=192, depth=24, num_heads=12, init_values=1e-5)
+
 model_args_xxs24 = dict(patch_size=16, embed_dim=192, depth=24, num_heads=4, init_values=1e-5)
 model_args_xxs36 = dict(patch_size=16, embed_dim=192, depth=36, num_heads=4, init_values=1e-5)
+
+def cait_xxs24_mh(**kwargs):
+    model = Cait(
+        embed_dim=model_args_xxs24_mh['embed_dim'], 
+        depth=model_args_xxs24_mh['depth'], 
+        num_heads=model_args_xxs24_mh['num_heads'], 
+        init_values=model_args_xxs24_mh['init_values'],
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
 
 def cait_xxs24(**kwargs):
     model = timm.models.cait.cait_xxs24_224(**kwargs)
